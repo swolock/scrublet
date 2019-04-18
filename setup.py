cec7464231a9ec9ebc38a3a04f4eb32f12064d3a
swolock <swolock@gmail.com>
2019-04-18 14:05:09 -0400

    Update setup.py description

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name = "scrublet",
     packages = ['scrublet'],
     package_dir={'': 'src'},
     version = '0.2',
     description = 'Doublet prediction in single-cell RNA-sequencing data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Samuel L. Wolock',
     author_email = 'swolock@g.harvard.edu',
-    url = 'https://github.com/swolock/scrublet',
-    download_url = 'https://github.com/swolock/scrublet/tarball/0.1',
+    url = 'https://github.com/allonkleinlab/scrublet',
     install_requires=['cython', 'numpy', 'scipy', 'scikit-learn', 'scikit-image', 'matplotlib', 'annoy', 'numba', 'pandas', 'umap-learn'],
     )
